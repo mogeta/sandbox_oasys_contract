6b006f9c3a31023c46152fff1e09f58f28bf9fa0
hiroshi mimori <muzigen@gmail.com>
2024-07-24 14:24:07 +0900

    Create HelloWorld.sol

---
+++ HelloWorld.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    string public message;
+
+    constructor() {
+        message = "Hello World!";
+    }
+
+    function setMessage(string memory _message) public {
+        message = _message;
+    }
+
+    function getMessage() public view returns (string memory) {
+        return message;
+    }
+}
